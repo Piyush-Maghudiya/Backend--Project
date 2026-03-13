904d10af0aaef572dc14426be99bbacc23d1f77d
Piyush-Maghudiya <maghudiyapiyush8206@gmail.com>
2026-03-13 15:40:21 +0530

    add util file and undestand app.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import dotenv from "dotenv";
     path: '/.env'
  })
  connectDb()
+ .then(()=>{
+    app.listen( process.env.PORT || 8000 ,()=>{
+      console.log(`server is running on port number :${process.env.PORT}`)
+    })
+ })
+ .catch((err)=>{
+   console.log("MONGODB connction faild !!",err)
+ })
