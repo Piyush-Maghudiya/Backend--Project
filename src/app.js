4d2153267ce3d7be619ec24a72a86071bfb8dd8f
Piyush-Maghudiya <maghudiyapiyush8206@gmail.com>
2026-05-02 10:49:44 +0530

    add subscriptionn controller and subscription router

--- src/app.js
+++ src/app.js
@@ -19,12 +19,14 @@ import userRouter from "./routes/user.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 // route decalre
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/comment",commentRouter)
 app.use("/api/v1/video",videoRouter)
-app.use("/api/v1/like",likeRouter)
-
-
+app.use("/api/v1/likes",likeRouter)
+app.use("/app/v1/playlist",playlistRouter)
+app.use("/api/v1/subcriptions",subscriptionRouter)
 export default app
 
